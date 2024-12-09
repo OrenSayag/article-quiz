51b3ae042aaa2017f4c86e0fb28ca4b617f6f507
Oren Sayag <orensayag500@gmail.com>
2024-12-09 17:47:56 +0200

    ♻️ cleaned article-quiz backend application. 🐳 modified docker-compose to include unstructured and db services. removed cache service.

--- apps/article-quiz/src/modules/app/app.module.ts
+++ apps/article-quiz/src/modules/app/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { QuizGeneratorModule } from '../quiz-generator/quiz-generator.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { RedisOptions } from '../../constants/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    CacheModule.registerAsync(RedisOptions),
-    QuizGeneratorModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- libs/prompt-generation/README.md
+++
@@ -1,7 +0,0 @@
-# prompt-generation
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build prompt-generation` to build the library.
