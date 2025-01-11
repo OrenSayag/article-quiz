df55e72c98ae2d61733e6ebb1639885bcb9f0937
Oren Sayag <orensayag500@gmail.com>
2025-01-11 21:01:06 +0200

    ✨ aq-83 -  get-quiz - type url - look that url is accessible (status 200) before gen quiz

--- apps/article-quiz/src/modules/quiz/methods/get-quiz.ts
+++ apps/article-quiz/src/modules/quiz/methods/get-quiz.ts
@@ -3,6 +3,7 @@ import { InputContent, Quiz } from '@article-quiz/shared-types';
 import { htmlToMd, urlToMd } from '@article-quiz/jina-ai';
 import { mdGenOpts } from '@article-quiz/utils';
 import { generateQuizClaude } from '@article-quiz/quiz-generator';
+import { BadRequestException } from '@nestjs/common';
 
 type Input = {
   inputContent: InputContent;
@@ -24,6 +25,9 @@ export const getQuiz = async ({
   });
   if (res !== 'not-found') {
     return res;
+  }
+  if (inputContent.contentType === 'url') {
+    inputContent.url = await validateUrl(inputContent.url);
   }
   const source = (() => {
     switch (inputContent.contentType) {
@@ -48,6 +52,7 @@ export const getQuiz = async ({
     }
   })();
   const start = new Date();
+
   const quiz = await generateQuizClaude({
     apiKey: claudeApiKey,
     mdContent: mdContent,
@@ -67,4 +72,28 @@ function extractTitle(mdContent: string) {
   const titleRegex = /^Title:\s*(.*)/m;
   const match = mdContent.match(titleRegex);
   return match ? match[1] : undefined;
+}
+
+async function validateUrl(url: string): Promise<string> {
+  const normalizedUrl = removeQueryParamsAndAnchors(url);
+  const { status } = await fetch(url);
+  const successfulStatus = status >= 200 && status < 300;
+  if (!successfulStatus) {
+    throw new BadRequestException(
+      `Invalid status received from url ${normalizedUrl}. Status ${status}`
+    );
+  }
+  return normalizedUrl;
+
+  function removeQueryParamsAndAnchors(urlString: string): string {
+    try {
+      const url = new URL(urlString);
+      url.search = ''; // Remove query parameters
+      url.hash = ''; // Remove the anchor
+      return url.toString();
+    } catch (error) {
+      console.error('Invalid URL:', error);
+      return urlString;
+    }
+  }
 }
